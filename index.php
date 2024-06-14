63c60989d7c06bc853750e3f6df99ece3e1de5a4
qwertyuiopas89 <cristiancuce00@gmail.com>
2024-06-14 11:37:27 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,150 @@
+</php
+//.1 Traccia 
+// Integer
+// Float
+// String
+// Boolean
+// A schermo,fai comparire il tipo di variabili
+// Trasforma quelle variabili in costanti
+
+$1text ="MArco";
+$text2 ="hai";
+$text3 ="sete";
+$text4 ="?";
+@text5 ="Perchè";
+$te-xt6="$text2";
+$text7 ="bevuto";
+$text8 ="tutto";
+
+var_dump ($text ," ",$text2," ",$text3," ",$text4," ",$text5," ",$te-xt6," "$text7," ",$text8," "
+
+const USERNAME ="CRISTIAN";
+
+$words1 = [
+'una';
+67,
+'vita',
+'colle',
+'mi',
+'rosso',
+[
+'oscura',
+'era',
+89,
+[
+ 'Nel',
+ 'mezzo',
+'cammin',
+ 'Nel',
+[
+ 'selva',
+ 'la',
+ [
+  'via',
+  'una',
+true,
+   ]
+  ],
+ ]
+],
+'ritrovai',
+'per',
+]
+ 'diritta',
+];
+//echo $results;
+
+$word2 = [
+
+'elemento1'=>25,89,
+'elemento2'=>'nostra'
+'elemento13'=[
+'virgilio',
+'smarrita',
+'che',
+ ]
+],
+
+
+$results= $words1 [6][3][1][1]." ".$words1 [6][3][0]." ".del"." ".$words1[6][1][3][0]
+"$words1[4]." ".$words1[6][5]." ".$words1[0]." ".$words1[6][3][1][2][0]
+finita:)";
+
+//echo $results;
+
+//traccia 4
+$users = [
+
+   ['name' => 'Cristian','gender' = 'NB']
+   ['name' => 'Caterina','gender' = 'F']
+   ['name' => 'Sushanto','gender' = 'M']
+   ['name' => 'Mohamed',gender'  = 'M']
+   ['name' => 'Elena','gender'  = 'F']
+   ['name` =< 'Alessandro','gender' ='NB']
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
